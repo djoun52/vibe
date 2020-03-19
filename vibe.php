156eb0db88da9d925657ce9791e159fa7f4040a1
djoun52 <33383124+djoun52@users.noreply.github.com>
2020-03-19 19:23:47 +0100

    push  dez fin de journé

---
+++ vibe.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div id="wrapper">
+        <header>
+            <section>
+                <a id="logo" href="">
+                    <img src=" ./images/Logo-Vibes-Collectif-le-Chateau.png " height: 100px alt="logo">
+                </a>
+                <nav>
+                    <a href=""> HOME </a>
+                    <a href=""> ABOUT    </a>
+                    <a href=""> GALLERY</a>
+                    <a href=""> TESTIMONIALS </a>
+                    <a href=""> CONTACT </a>
+                    <a href=""> PAGES </a>
+                </nav>
+            </section>
+            <section>
+                <h1>
+                    HTML TEMPLATE FOR CREATIVE FOLKS AND DESIGN AGENCIES
+                </h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore facere, et fugiat ipsam voluptate commodi! Possimus, earum aliquam. Molestias quos eveniet rem. Sit, perferendis dignissimos numquam minus qui laudantium fugiat.
+                </p>   
+                    <a href="" class="bouton"> READ MORE </a>      
+    
+            </section>
+        </header>
+        <main>
+            <section>
+            
+                <article>
+                    <h2>Why choose us?</h2>
+                    <p>
+                       <span class="gras">>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis quisquam voluptatibus sequi saepe ipsam</span>
+                    </p>
+                    <p class="caraPetit">
+                    Neque nisi maiores excepturi ad omnis, asperiores. Numquam quae autem et tempore fuga doloremque laudantium aspernatur? 
+                    </p>
+                </article>
+                
+                <article>
+                    <h1>What you get?</h1>
+                </article>
+
+                <article>
+                   
+                </article>   
+            </section>
+            
+            <section>
+
+            </section>
+            
+            <section>
+                <h2>TESTIMONIALS</h2>
+            </section>
+            
+            <section>
+                <h>NEW HORIZONS AWAIT</h1>
+            </section>
+                 <h1>CONTACT</h1>
+            <section>
+
+            </section>
+        </main>
+
+        <footer>
+
+
+        </footer>
+    </div>
+
+</body>
+
+</html>
